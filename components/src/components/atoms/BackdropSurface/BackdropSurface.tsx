c23e02a42175e9e8d36fb269648ce1191dbf6b08
Leon Talbert <leon.talbert@gmail.com>
2022-02-26 12:03:05 +0800

    BackdropSurface

--- components/src/components/atoms/BackdropSurface/BackdropSurface.tsx
+++ components/src/components/atoms/BackdropSurface/BackdropSurface.tsx
@@ -1,8 +1,22 @@
-import * as React from 'react'
+import React, { ReactElement } from 'react'
+import styled from 'styled-components'
 
-import { Box, BoxProps } from '../Box'
-import * as styles from './styles.css'
+import { tokens } from '@/src/tokens'
 
-export const BackdropSurface = ({ ...props }: BoxProps) => (
-  <Box className={styles.backdrop} {...props} />
-)
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  position: fixed;
+  overflow: hidden;
+  top: 0;
+  ${(p) => `
+    backgroundColor: ${tokens.shades[p.theme.mode].backgroundHideFallback};
+    
+    @supports (-webkit-backdrop-filter: none) or (backdrop-filter: none) {
+    backdrop-filter: blur(30px);
+    background-color: ${tokens.shades[p.theme.mode].backgroundHide};
+  }
+  `}
+`
+
+export const BackdropSurface = (props: ReactElement) => <Container {...props} />
