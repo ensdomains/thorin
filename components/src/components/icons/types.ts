1427dfa05374eb135b5dd2badf75f885dfd3be24
Leon Talbert <leon.talbert@gmail.com>
2022-03-03 16:17:58 +0800

    delete icons

--- components/src/components/icons/types.ts
+++
@@ -1,7 +0,0 @@
-import * as React from 'react'
-
-import { AllOrNone } from '../../types'
-
-export type OptionalTitle = AllOrNone<{ title: string; titleId: string }>
-
-export type IconProps = React.SVGProps<SVGSVGElement> & OptionalTitle
