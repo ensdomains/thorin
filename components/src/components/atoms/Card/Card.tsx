792b6ac204f2e9f259216913351dc997c805d5a9
Leon Talbert <leon.talbert@gmail.com>
2022-03-22 14:32:20 +0800

    Card fixes

--- components/src/components/atoms/Card/Card.tsx
+++ components/src/components/atoms/Card/Card.tsx
@@ -5,16 +5,28 @@ import { largerThan } from '@/src/utils/responsiveHelpers'
 import { tokens } from '@/src/tokens'
 
 const Container = styled.div<{ dark: boolean; shadow?: boolean }>`
+  padding: ${tokens.space['6']};
+  border-radius: ${tokens.radii['2xLarge']};
+
+  ${largerThan.lg`
+    border-radius: ${tokens.radii['3xLarge']};
+  `}
+
+  ${({ dark }) =>
+    dark
+      ? `background-color: ${tokens.colors.base.black};`
+      : `background-color: ${tokens.colors.base.white};`}
+
   ${(p) => `background-color: ${p.dark ? `black` : `white`}`};
   ${(p) =>
     !p.dark &&
     p.shadow &&
     `
-        boxShadow: 0px 0px ${tokens.radii['2xLarge']} rgb('0,0,0', '0.1');
+        box-shadow: 0px 0px ${tokens.radii['2xLarge']} rgba(0,0,0,0.1);
         border-radius: ${tokens.radii['2xLarge']};
         
         ${largerThan.lg`
-            boxShadow: 0px 0px ${tokens.radii['3xLarge']} rgb('0,0,0', '0.1');
+            box-shadow: 0px 0px ${tokens.radii['3xLarge']} rgba(0,0,0,0.1);
             border-radius: ${tokens.radii['3xLarge']};
         `}
     `}
