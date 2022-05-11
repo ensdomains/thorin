c08cdb7095d3ecbea00185132561d20aa7882772
storywithoutend <storywithoutend@gmail.com>
2022-05-11 10:43:46 +0800

    fix comments 2

--- docs/playroom.config.js
+++ docs/playroom.config.js
@@ -32,23 +32,6 @@ module.exports = {
             },
           },
         },
-        {
-          test: /\.svg$/,
-          issuer: /\.[jt]sx?$/,
-          use: [
-            { loader: '@svgr/webpack', options: { icon: true } },
-            { loader: 'file-loader', options: { name: '[name].[ext]' } },
-          ],
-        },
-        {
-          test: /\.mdx$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {},
-            },
-          ],
-        },
       ],
     },
     resolve: {
