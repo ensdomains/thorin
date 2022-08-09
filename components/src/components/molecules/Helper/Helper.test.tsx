d994fda5ab30ca75c0f4f401d121966c0236aadc
tate <tate@ens.domains>
2022-08-09 11:19:25 +1000

    wrapped test with themeprovider

--- components/src/components/molecules/Helper/Helper.test.tsx
+++ components/src/components/molecules/Helper/Helper.test.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react'
 
+import { ThemeProvider } from 'styled-components'
+
 import { cleanup, render } from '@/test'
 
 import { Helper } from './Helper'
+import { lightTheme } from '@/src'
 
 describe('<Helper />', () => {
   afterEach(cleanup)
 
   it('renders', () => {
-    render(<Helper>Test helper</Helper>)
+    render(
+      <ThemeProvider theme={lightTheme}>
+        <Helper>Test helper</Helper>
+      </ThemeProvider>,
+    )
   })
 })
