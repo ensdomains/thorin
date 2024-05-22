bc843a3e455108fdb108bb8a3aa9c05b8cfb3a4f
storywithoutend <storywithoutend@gmail.com>
2024-05-22 11:09:27 +0800

    fix lint errors

--- docs/src/playroom/types.ts
+++ docs/src/playroom/types.ts
@@ -1,5 +1,13 @@
-import { Snippets as PlayroomSnippets } from 'playroom'
+// import { Snippets as PlayroomSnippets } from 'playroom'
 import { Optional } from 'utility-types'
+
+interface PlayroomSnippet {
+  group: string
+  name: string
+  code: string
+}
+
+type PlayroomSnippets = PlayroomSnippet[]
 
 export type Snippet = Omit<
   Optional<PlayroomSnippets[number], 'group'>,
