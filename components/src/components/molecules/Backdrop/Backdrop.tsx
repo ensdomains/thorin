dcd8a358fd6fc2b847a1c6cbf302a8343617c299
v1rtl <hi@v1rtl.site>
2024-08-27 15:42:57 +0300

    fix backdrop

--- components/src/components/molecules/Backdrop/Backdrop.tsx
+++ components/src/components/molecules/Backdrop/Backdrop.tsx
@@ -7,9 +7,7 @@ import { BackdropSurface } from '../../atoms/BackdropSurface'
 
 type Props = {
   /** A function that renders the children nodes */
-  children: (renderProps: {
-    state: TransitionState['status']
-  }) => React.ReactNode
+  children: (renderProps: { state: TransitionState }) => React.ReactNode
   /** An element that provides backdrop styling. Defaults to BackdropSurface component. */
   surface?: React.ElementType
   /** A event fired when the background is clicked. */
@@ -32,7 +30,7 @@ export const Backdrop = ({
   open,
   renderCallback,
 }: Props) => {
-  const [{ status: state }, toggle] = useTransition({
+  const [state, toggle] = useTransition({
     timeout: {
       enter: 50,
       exit: 300,
@@ -80,21 +78,6 @@ export const Backdrop = ({
           modifyBackdrops(-1)
         }
       }
-      modifyBackdrops(1)
-      return () => {
-        const top = parseFloat(style.top || '0') * -1
-        if (currBackdrops() === 1) {
-          setStyles('', '', '')
-          window.scroll({
-            top,
-          })
-        }
-        modifyBackdrops(-1)
-        toggle(!toggleValue)
-      }
-    }
-    return () => {
-      toggle(!toggleValue)
     }
     return () => {
       toggle(false)
