e258d905b51e9065fb38d9293cb5d16e29e59769
v1rtl <hi@v1rtl.site>
2024-08-29 14:43:33 +0300

    export the type used in manager-app

--- components/src/index.ts
+++ components/src/index.ts
@@ -1,7 +1,7 @@
 export * from './components'
 export * as Components from './components'
 export { tokens, baseTheme, lightTheme, darkTheme } from './tokens'
-export type { DefaultTheme, EmptyObject, Accent, Mode } from './types'
+export type { DefaultTheme, EmptyObject, Accent, Mode, ReactNodeNoStrings } from './types'
 export type { Hue, Colors, Space } from './tokens'
 export * from './css/theme.css'
 export * from './tokens/color3'
