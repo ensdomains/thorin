6799a07a39623eb213b261ddf3b72900b47c62c5
v1rtl <hi@v1rtl.site>
2024-09-27 16:05:14 +0300

    feat: dark theme support in docs

--- docs/src/pages/_app.tsx
+++ docs/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import type { AppProps } from 'next/app'
 import { MDXProvider } from '@mdx-js/react'
 
+import type { Mode } from '@ensdomains/thorin'
 import { ThemeProvider } from '@ensdomains/thorin'
 import '@ensdomains/thorin/dist/style.css'
 import { MDX } from '~/components'
@@ -9,6 +10,14 @@ import { getLayout as getDocsLayout } from '~/layouts/docs'
 import '~/styles/globalStyles.css'
 import '../styles/styles.css'
 import type { GetLayout, NextComponentType } from 'next'
+
+declare global {
+  interface Window {
+    __theme: Mode
+    __setPreferredTheme: (theme: Mode) => void
+    __onThemeChange: (theme: Mode) => void
+  }
+}
 
 const App = ({ Component, pageProps }: AppProps) => {
   const getLayout = (Component as NextComponentType & { getLayout: GetLayout }).getLayout || getDocsLayout
@@ -18,7 +27,12 @@ const App = ({ Component, pageProps }: AppProps) => {
       {/* <Head /> */}
       {/* Prevent theme flash */}
       <MDXProvider components={MDX}>
-        <ThemeProvider>{getLayout(<Component {...pageProps} />)}</ThemeProvider>
+        <ThemeProvider
+          onThemeChange={mode => window.__setPreferredTheme(mode)}
+          defaultMode={typeof window !== 'undefined' ? window.__theme : 'light'}
+        >
+          {getLayout(<Component {...pageProps} />)}
+        </ThemeProvider>
       </MDXProvider>
     </>
   )
