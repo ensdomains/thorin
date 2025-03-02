400b9247de6bf5864e6fbf6da814e9a8592ca83d
v1rtl <hi@v1rtl.site>
2025-03-02 14:31:37 +0200

    remove warning about globs

--- docs/next.config.mjs
+++ docs/next.config.mjs
@@ -1,14 +1,10 @@
 import { createVanillaExtractPlugin } from '@vanilla-extract/next-plugin'
 import path from 'node:path'
 const withVanillaExtract = createVanillaExtractPlugin({ identifiers: 'short' })
-
-import { globSync } from 'node:fs'
+import { readdirSync } from 'node:fs'
 
 const getComponentPaths = category =>
-  globSync(`./src/reference/mdx/${category}/*.docs.mdx`, {
-    cwd: process.cwd(),
-
-  })
+  readdirSync(`./src/reference/mdx/${category}`)
     .map((x) => {
       const name = path.basename(x, '.docs.mdx')
       const route = `/components/${category}/${name}`
